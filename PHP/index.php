35b355ae5441747a7dce6b6f4d2fd0fb14182af2
fabiodigiacomopepe <fabiodgp20@gmail.com>
2023-06-22 17:37:09 +0200

    Add axios (get)

--- PHP/index.php
+++ PHP/index.php
@@ -1,3 +1,22 @@
 <?php
+    header("Access-Control-Allow-Origin: http://localhost:5173");
+    header("Access-Control-Allow-Headers: X-Requested-With");
+    header('Content-Type: application/json');
 
+    $toDoList = [
+        [
+            'name' => 'compra pane',
+            'done' => true
+        ],
+        [
+            'name' => 'compra latte',
+            'done' => false
+        ],
+    ];
+
+    echo json_encode($toDoList);
+
+    /* $data = $_POST;
+    $encData = json_encode($data);
+    echo $encData; */
 ?>
--- Vue/src/App.vue
+++ Vue/src/App.vue
@@ -1,6 +1,6 @@
 <script>
 import { store } from './store.js'
-/* import axios from 'axios'       // SE HO INSTALLATO AXIOS */
+import axios from 'axios'
 import AppHelloWorld from './components/AppHelloWorld.vue'
 
 
@@ -10,24 +10,69 @@ export default {
   },
   data() {
     return {
-      store
+      store,
+      tasks: [],
+      nuovaTask: {
+        name: "",
+        done: false
+      }
     }
   },
   methods: {
+    invia() {
 
+      const url = "http://localhost/22-06%20-%20PHP%20ToDo%20List%20JSON/php-todo-list-json/PHP/";
+      const data = this.nuovaTask;
+
+      const headers = {
+        headers: { 'Content-Type': 'multipart/form-data' }
+      };
+
+      axios
+        .post(url, data, headers)
+        .then(
+          risultato => {
+            console.log(risultato.data);
+          }
+        )
+    }
   },
-  created() {
-
+  mounted() {
+    axios
+      .get("http://localhost/22-06%20-%20PHP%20ToDo%20List%20JSON/php-todo-list-json/PHP/")
+      .then(
+        risultato => {
+          this.tasks = risultato.data;
+        }
+      )
   }
 }
 </script>
 
 <template>
-  <AppHelloWorld />
+  <form @submit.prevent=invia()>
+    <label for="task">Inserisci task </label>
+    <input type="text" name="newTask" id="newTask" v-model="nuovaTask.name">
+    <input type="submit" value="INVIA">
+  </form>
+
+  <ul>
+    <li v-for="(task, index) in tasks" :key="index">
+      {{ task.name }}
+    </li>
+  </ul>
 </template>
 
 <style lang="scss">
 @use './styles/general.scss' as *;
 @use './styles/partials/variables.scss' as *;
 @use './styles/partials/mixins.scss' as *;
+
+body {
+  padding: 30px;
+}
+
+ul {
+  margin: 30px 0 0 15px;
+}
 </style>
