fa386764448048b31ed04ae50081289137b05504
fabiodigiacomopepe <fabiodgp20@gmail.com>
2023-06-22 19:15:07 +0200

    Add style

--- Vue/src/App.vue
+++ Vue/src/App.vue
@@ -60,19 +60,24 @@ export default {
 </script>
 
 <template>
-  <form @submit.prevent=invia()>
-    <label for="task">Inserisci task </label>
-    <input type="text" name="newTask" id="newTask" v-model="newTask.name">
-    <input type="submit" value="INVIA">
-  </form>
+  <div class="container">
+    <h1>Todo List</h1>
+    <div class="list">
+      <ul>
+        <li v-for="(task, index) in tasks" :key="index" @click.stop="cambiaStato(task)">
+          <span :class="((task.done === 'true') ? 'barrato' : '')">
+            {{ task.name }}
+          </span>
+          <i @click.stop=" rimuoviItem(index)" class="fa-solid fa-xmark"></i>
+        </li>
+      </ul>
+    </div>
 
-  <ul>
-    <li v-for="(task, index) in tasks" :key="index" :class="((task.done === 'true') ? 'barrato' : '')"
-      @click.stop="cambiaStato(task)">
-      {{ task.name }}
-      <i @click.stop="rimuoviItem(index)" class="fa-solid fa-xmark"></i>
-    </li>
-  </ul>
+    <form @submit.prevent=invia()>
+      <input type="text" name="newTask" id="newTask" v-model="newTask.name" placeholder="Inserisci elemento...">
+      <input type="submit" id="submit" value="Inserisci">
+    </form>
+  </div>
 </template>
 
 <style lang="scss">
@@ -80,27 +85,67 @@ export default {
 @use './styles/partials/variables.scss' as *;
 @use './styles/partials/mixins.scss' as *;
 
-body {
-  padding: 30px;
-}
+.container {
+  width: 50%;
+  margin: 0 auto;
+  margin-top: 40px;
+  text-align: center;
 
-ul {
-  margin: 30px 0 0 15px;
-}
+  h1 {
+    font-size: 80px;
+    text-align: center;
+    color: #6b747c;
+  }
 
-.barrato {
-  text-decoration: line-through;
-}
+  .list {
+    background-color: #ffffff;
+    color: black;
+    border-radius: 5px;
 
-li {
-  cursor: pointer;
-  margin-bottom: 10px;
-}
+    li {
+      cursor: pointer;
+      border: 1px solid gray;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 5px 20px;
+      font-size: 25px;
 
-i {
-  margin-left: 30px;
-  background-color: red;
-  color: white;
-  padding: 2px 5px;
+      .barrato {
+        text-decoration: line-through;
+      }
+
+      i {
+        margin-left: 30px;
+        background-color: red;
+        color: white;
+        padding: 2px 5px;
+        text-decoration: none;
+      }
+    }
+  }
+
+  form {
+    width: 100%;
+    margin-top: 20px;
+    font-size: 35px;
+    display: flex;
+    align-items: center;
+
+    #newTask {
+      padding: 5px 15px;
+      width: 100%;
+    }
+
+    #submit {
+      padding: 5px 15px;
+      background-color: #001632;
+      color: #b78417;
+      border: 2px solid #b78417;
+      border-radius: 5px;
+      font-weight: bold;
+    }
+
+  }
 }
 </style>
